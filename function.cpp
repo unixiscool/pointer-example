1f663b76be8b78801e35d58a7fa16e792e3cbc1e
unixiscool <unix.keng@gmail.com>
2023-11-13 21:55:10 +0200

    0.1

--- function.cpp
+++ function.cpp
@@ -1,5 +1,11 @@
 #include "header.h"
+#include <cstdint>
 
-int Player(int* health, int* energy, int* count) {
-  return *count;
+int Player(int *health, int *energy, int *count) { return *count; }
+
+int Enemy(int *health, int *energy, int *count) { return *count; }
+
+int CreateEnemy(int *health, int *energy, int *count,
+  int (*pP)(int *, int *, int *)) {
+    return pP(health, energy,count);
 }
--- source.cpp
+++ source.cpp
@@ -1,6 +1,12 @@
 #include "header.h"
 int Player(int* health, int* energy, int* count);
-  
+int Enemy(int *health, int *energy, int *count);
+int CreateEnemy(int *health, int *energy, int *count,
+		int (*pP)(int *, int *, int *));
+
+#define MIN_HEALTH 0
+#define MAX_HEALTH 200
+
 int main() {
   //structure begin
   int health = 150;
@@ -9,9 +15,9 @@ int main() {
   //structure end
   int player[3] = {health,energy,count_arg}; //array of player
   
-  int (*p)(int*,int*,int*) = &Player; //указатель на функцию
+  int (*p)(int*,int*,int*) = Player; //указатель на функцию
   p(&player[1], &player[2], &player[3]); //передаем параметры
-  printf("%d\n", player[0]); //выводим 0 элемент и получаем "150" в терминал, т.е число energy
-  
+  printf("%d\n", player[2]); //выводим 0 элемент и получаем "150" в терминал, т.е число energy
+  int (*pCreateEnemy)(int *, int *, int *, int (*)(int *, int *, int *)) = CreateEnemy;
   return 0; 
 }
