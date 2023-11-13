9b9939d6d6ae9a28ffe9868e474876b1f71788dd
unixiscool <unix.keng@gmail.com>
2023-11-13 23:00:13 +0200

    Add battle system

--- function.cpp
+++ function.cpp
@@ -1,11 +1,31 @@
 #include "header.h"
+#include <algorithm>
 #include <cstdint>
+#include <random>
 
 int Player(int *health, int *energy, int *count) { return *count; }
 
 int Enemy(int *health, int *energy, int *count) { return *count; }
 
-int CreateEnemy(int *health, int *energy, int *count,
+bool Battle(int player[], int enemy[], char choose, bool win) {
+  while (true) {
+  printf("Что будем делать??\n");
+  std::cin >> choose;
+  if (choose == 'a') {
+    enemy[0] -= 10;
+    printf("%d\n", enemy[0]);
+    if (enemy[0] == 0) {
+      printf("Вы победили!\n");
+      return win = true;
+    }
+  }
+  else if (choose == 'q') {
+   break;
+     }
+  }
+ 
+}
+/*int CreateEnemy(int *health, int *energy, int *count,
   int (*pP)(int *, int *, int *)) {
     return pP(health, energy,count);
-}
+    }*/
--- source.cpp
+++ source.cpp
@@ -1,23 +1,42 @@
 #include "header.h"
 int Player(int* health, int* energy, int* count);
 int Enemy(int *health, int *energy, int *count);
-int CreateEnemy(int *health, int *energy, int *count,
-		int (*pP)(int *, int *, int *));
-
+/*int CreateEnemy(int *health, int *energy, int *count,
+                int (*pP)(int *, int *, int *));
+*/
+// int CreateEnemy(int (*pP)(int *, int *, int *));
+// bool Battle(int* player, int* enemy, int enemyDamage, int playerDamage, char*
+// choose);
+bool Battle(int* player, int* enemy, char choose, bool win);
 #define MIN_HEALTH 0
 #define MAX_HEALTH 200
-
+bool (*B)(int *, int *, char, bool) = Battle;
+int (*pE)(int *, int *, int *) = Enemy;
+char choose;
+bool victory;
 int main() {
-  //structure begin
+  //structure enemy begin
+  int healthE = 50;
+  int energyE = 25;
+  int count_argE = 3;
+  int enemy_D = 5;
+  //structure enemy end
+  
+  //structure player begin
   int health = 150;
   int energy = 100;
   int count_arg = 3;
-  //structure end
+  int player_D = 10;
+  //structure player end
   int player[3] = {health,energy,count_arg}; //array of player
+  int enemy[3] = {healthE, energyE, count_argE};
   
   int (*p)(int*,int*,int*) = Player; //указатель на функцию
-  p(&player[1], &player[2], &player[3]); //передаем параметры
-  printf("%d\n", player[2]); //выводим 0 элемент и получаем "150" в терминал, т.е число energy
-  int (*pCreateEnemy)(int *, int *, int *, int (*)(int *, int *, int *)) = CreateEnemy;
+  p(&player[0], &player[1], &player[2]); //передаем параметры
+  
+  pE(&enemy[0], &enemy[1], &enemy[2]);
+  printf("%d\n", enemy[0]); //выводим 0 элемент и получаем "150" в терминал, т.е число energy
+  //int (*pCreateEnemy)(int *, int *, int *, int (*)(int *, int *, int *)) = CreateEnemy;
+  B(player, enemy, choose, victory);
   return 0; 
 }
